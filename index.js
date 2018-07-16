37b61d9cfe35af58981552f88758b606a57dd2e0
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-16 18:43:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function getFirstSelector(selector){
+  return  document.querySelector(selector)
+}
 
+function nestedTarget(){
+  var el = document.querySelector('div#nested.target')
+  return el
+}
