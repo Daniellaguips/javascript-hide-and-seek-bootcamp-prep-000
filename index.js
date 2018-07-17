6a423e966ba8ba300cc1ffb81f5d30614ccdf799
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-17 19:48:34 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
- const idul = document.getElementById('nested').querySelectorAll('div.target')
+ const idul = document.getElementById('nested').querySelector('.target')
  return idul
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node')
+  var test = deep.children[0]
+  while (test){
+    deep = test
+    test = deep.children[0]
+  } 
+  return test
+ // for (let i = 0; i<deep.length; i++){
+   //  test = deep[i].querySelector("div")
+//  }
+}
+
+function increaseRankBy(n){
+  var rank = document.getElementById("app")
+ var test =  document.querySelectorAll("ul.ranked-list li")
+ var nextNode
+ for (let i = 0; i<test.length;i++){
+   test[i].innerHTML = parseInt(test[i].innerHTML)+n
+ }
 }
